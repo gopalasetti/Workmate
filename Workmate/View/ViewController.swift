b74a94db5229eee1dcc522f309b6f26565105896
Siva Kumar Gopalasetti <gsiva@sph.com.sg>
2020-02-25 15:38:01 +0800

    Initial commit. Project Created.

---
+++ Workmate/View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Workmate
+//
+//  Created by gsiva on 25/2/20.
+//  Copyright © 2020 gsiva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
